980399c82a9635b0b62a440fd5f5721e6671ee52
Shehryar Ahmed <shehryar.ahmed@utexas.edu>
2018-01-15 21:37:20 -0600

    Add files via upload
    Main program is finished in O(N) time, it also prompts the user if they would like to include a company more than once. Finished, except it doesn't print the names of the companies that are chosen.

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,13 +1,14 @@
 #https://www.geeksforgeeks.org/knapsack-problem/ ->knapsack problem
 # I USED NOTES FROM A PREVIOUS CLASS, WILL UPLOAD THEM WHEN I FIND THEM
 
-company = ['A','B','C','D','E','F','G','H','I','J']
+company = ['A','B','C','D','E','F','G','H','I','J'] #company names
 amount = [1,2,3,4,5,6,7,8,9,10] 
 price = [1,5,8,9,10,17,17,20,24,30]
 length = len(company)
 maxAmount = 30
 wantthis = length
 myList = [[0 for j in range(maxAmount+1)] for i in range(length+1)]
+chosenCompanies = []
 
 def test(am,pr, le, maxAm): #return maax value that can be returned
     for i in range(le+1):
@@ -15,7 +16,8 @@ def test(am,pr, le, maxAm): #return maax value that can be returned
             if i==0 or j == 0:     #if either is 0 then either the max price has been reached (so dontt go on), or there are no more elements
                 myList[i][j] = 0
             elif am[i-1]  <= j:  
-                myList[i][j] = max(pr[i-1]+myList[i-1][j-am[i-1]],myList[i-1][j])
+                myList[i][j] = max(pr[i-1]+myList[i-1][j-am[i-1]], myList[i-1][j])
+
             else:
                 myList[i][j] = myList[i-1][j]
     return myList[wantthis][maxAm]
@@ -27,13 +29,13 @@ def test(am,pr, le, maxAm): #return maax value that can be returned
         else:
             return withoutIt
             '''
-'''def test2(am,pr, le, maxAm):
+def test2(am,pr, le, maxAm):
     list2 = [0 for w in range(maxAm+1)]
-    for i in range(le+1):
-        for j in range(maxAm+1):
+    for i in range(maxAm+1):
+        for j in range(le):
             if am[j] <= i:
-                list2[i] = max(pr[j]+list2[i-am[j]],list2[i])
-    return list2[maxAm]'''
+                list2[i] = max(pr[j]+list2[i-am[j]], list2[i])
+    return list2[maxAm]
 
 
 
